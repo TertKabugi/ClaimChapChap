47b2703f1799344c6cc5a73a6d54edf65b1962c0
Tertius Kabugi <kingkabugi@gmail.com>
2023-03-21 17:08:26 +0300

    splash screen...

---
+++ app/src/main/java/com/example/claimchapchap/onboarding/ViewPagerFragment.kt
@@ -0,0 +1,36 @@
+package com.example.claimchapchap.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.claimchapchap.R
+import com.example.claimchapchap.onboarding.screens.*
+//import kotlinx.androidx.synthetic.main.fragment_view_pager.view.*
+
+class ViewPagerFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_view_pager, container, false)
+
+        val fragmentList = arrayListOf<Fragment>(
+            ScreenOne(),
+            ScreenTwo(),
+            ScreenThree(),
+            ScreenFour(),
+            ScreenFive()
+        )
+        val adapter = ViewPagerAdapter(
+            fragmentList,
+            requireActivity().supportFragmentManager,
+            lifecycle
+        )
+
+        return view
+    }
+}

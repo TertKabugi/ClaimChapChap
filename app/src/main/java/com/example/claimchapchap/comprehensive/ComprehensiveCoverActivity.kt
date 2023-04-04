915ddc8ea07efbf47c6be184a4364122696304b1
Tertius Kabugi <kingkabugi@gmail.com>
2023-04-04 10:24:40 +0300

    comprehensive covers!!

---
+++ app/src/main/java/com/example/claimchapchap/comprehensive/ComprehensiveCoverActivity.kt
@@ -0,0 +1,12 @@
+package com.example.claimchapchap.comprehensive
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.claimchapchap.R
+
+class ComprehensiveCoverActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_comprehensive_cover)
+    }
+}
